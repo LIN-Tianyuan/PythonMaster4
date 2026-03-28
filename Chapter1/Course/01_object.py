353f4239f89a7fc6800d3399f92ef57ea969063f
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2026-03-28 14:47:16 +0100

    week 1

---
+++ Chapter1/Course/01_object.py
@@ -0,0 +1,14 @@
+# Créer un class
+class Student:
+    name = None
+
+# Création d'objets basés sur des classes
+stu_1 = Student()
+stu_2 = Student()
+
+# Attribution d'une valeur d'attribut name à l'objet stu_1
+stu_1.name = "Léon"
+stu_2.name = "Lucie"
+
+
+
